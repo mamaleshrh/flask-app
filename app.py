1d8503a1ccb8414e6d61796f60655517159cf81f
mamaleshrh <mamaleshhake@gmail.com>
2024-08-15 00:20:21 +0530

    h

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+from rating import get_difficulty
+import requests
 app = Flask(__name__)
 
 @app.route("/")
 def start():
     return "home page mamalesh"
+
+
+@app.route('/get-data', methods=['GET'])
+def get_data():
+    # Access query parameters using request.args
+    key = request.args.get('key')
+    return jsonify({"received_key": key})
+
 
 @app.route("/mamalesh")
 def mbsa():
---
+++ rating.py
@@ -0,0 +1,38 @@
+import requests
+import cloudscraper
+
+
+
+def get_difficulty():
+    try:
+        LINK = input("Problem link: ")
+        
+        scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
+        # Or: scraper = cloudscraper.CloudScraper()  # CloudScraper inherits from requests.Session
+        # print(scraper.get(LINK).text)  # => "<!DOCTYPE html><html><head>..."
+	
+	
+        f = scraper.get(LINK)
+        # print(f.text)
+        ind = f.text.find('title="Difficulty')
+        while(f.text[ind] != '*'):
+            ind += 1
+        ind += 1
+        rating = ""
+        while(f.text[ind] != '\r'):
+            rating += f.text[ind]
+            ind += 1
+        rating = int(rating)
+        difficulties = [1199, 1399, 1599, 1899, 2099, 2399, 10000]
+        colors = ["", "\u001b[38;5;10m", "\u001b[38;5;14m",
+                "\u001b[38;5;25m", "\u001b[38;5;99m", "\u001b[38;5;3m", "\u001b[38;5;9m"]
+        for i in range(len(difficulties)):
+            if(rating < difficulties[i]):
+                print("Rating: " + '\033[1m' + colors[i] + str(rating))
+                break
+    except Exception as e:
+        print("No rating found")
+
+
+if(__name__ == "__main__"):
+    get_difficulty()
